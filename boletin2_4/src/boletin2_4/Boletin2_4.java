7363eb788f60073271b55b3d12768415390fb845
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-10-10 12:35:30 +0200

    Cambio en el orden de las formulas

--- boletin2_4/src/boletin2_4/Boletin2_4.java
+++ boletin2_4/src/boletin2_4/Boletin2_4.java
@@ -10,14 +10,9 @@ public class Boletin2_4 {
         int b100, b20, b5, m1, t, r;
         System.out.println("Introduzca cantidad exacata de €");
         t= ler.nextInt();
-        b100=t/100; //cojemos el cociente como resultado para la cantidad de villetes, tras eso calculamos el resto y se lo restamos al total.
-        r=t%100;
-        t=t-(t-r);
-        b20=t/20;
-        r=t%20;
-        t=t-(t-r);
-        b5=t/5;
-        r=t%5;
+        b100=t/100;r=t%100; t=t-(t-r); //cojemos el cociente como resultado para la cantidad de villetes, tras eso calculamos el resto y se lo restamos al total.
+        b20=t/20;r=t%20;t=t-(t-r);
+        b5=t/5;r=t%5;
         m1=r;
         System.out.println("cantidad de billestes de 100€= " + b100 +"\ncantidad de billetes de 20€= " + b20 + "\ncantidad de billetes de 5€= " + b5 + "\ncantidad de monedas de 1€= "+m1);
              
